581e03993b132269c3fed20389bfff934033b262
raaviaggarwal00294@gmail.com <raaviaggarwal00294@gmail.com>
2026-08-21 16:06:10 +0530

    error changes for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,31 +1,49 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+//   build: {
+//     rolldownOptions: {
+//       output: {
+//         codeSplitting: {
+//           groups: [
+//             {
+//               name: 'vendor',
+//               test: /node_modules/,
+//               priority: 1,
+//               maxSize: 400 * 1024,
+//               minSize: 60 * 1024,
+//             },
+//           ],
+//         },
+//       },
+//     },
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          groups: [
-            {
-              name: 'vendor',
-              test: /node_modules/,
-              priority: 1,
-              maxSize: 400 * 1024,
-              minSize: 60 * 1024,
-            },
-          ],
-        },
       },
     },
   },
